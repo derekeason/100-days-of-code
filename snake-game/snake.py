ff01fa654bf9c6481249f47ea0add4c2e5636640
Derek Eason <daevelopd@gmail.com>
2021-10-27 22:19:09 -0400

    added food and scoreboard to game

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -17,18 +17,23 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle('square')
-            new_segment.color('white')
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle('square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(MOVE_DISTANCE)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
